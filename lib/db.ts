03f6ab7d61028b81f8dea7b0263df5168f4930ec
Iván Monte <ivanmonte07.93@gmail.com>
2025-04-07 00:15:04 -0300

    modificacion en db.ts con la db url

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,8 @@
 import { Pool } from 'pg';
+ 
+ const pool = new Pool({
+   connectionString: process.env.DATABASE_URL,
 
-// Conexión directa SIN lógica de entornos (la controlarás desde .env)
-const pool = new Pool({
-  connectionString: 
-    process.env.DATABASE_URL_PRODUCTION ||  // Usa producción si existe
-    process.env.DATABASE_URL_PREVIEW ||    // Si no, usa preview
-    process.env.DATABASE_URL,              // Si no, usa local (main)
-  ssl: { rejectUnauthorized: false }       // Neon requiere SSL
-});
-
-export default pool;
+ });
+ 
+ export default pool;
